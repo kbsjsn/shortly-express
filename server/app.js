8940f5fb97025d303f43e56b75dad4af8e30f7dd
kbsjsn <shin.kirk@gmail.com>
2019-06-21 17:20:41 -0700

    Completed setting up Usernames and passwords [passed 14 tests], moving on to Sessions and cookies

--- server/app.js
+++ server/app.js
@@ -17,67 +17,124 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
-
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    console.log('this is url', req.body.url);
+    // checks if url is valid, if not send 404
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+    // returns what name tied to URL (type of string)
+    return models.Links.get({ url })
+      // if the link exists, we don't do anything
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        // if it doesn't exist then get title
+        return models.Links.getUrlTitle(url);
+      })
+      // then takes url title and creates a line in the links table
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
 
+app.post('/signup',
+  (req, res, next) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    return models.Users.get({ username })
+      .then(user => {
+        if (user) {
+          throw user;
+        }
+        return models.Users.create({ username, password });
+      })
+      .then(() => {
+        res.redirect('/');
+        res.status(200).send();
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(() => {
+        res.redirect('/signup');
+        res.status(200).send();
+      });
+  });
+
+app.post('/login',
+  (req, res, next) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    // console.log(username);
+
+    return models.Users.get({ username })
+      .then(user => {
+        if (!user) {
+          throw user;
+        }
+        return models.Users.compare(password, user.password, user.salt);
+      })
+      .then((isValidPassword) => {
+        if (!isValidPassword) {
+          throw isValidPassword;
+        }
+        res.redirect('/');
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(() => {
+        res.redirect('/login');
+        res.status(200).send();
+      });
+  });
 
 
 /************************************************************/
