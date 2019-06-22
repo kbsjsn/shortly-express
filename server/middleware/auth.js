84a919891aa1aecd47578931d21d6c347c470efc
kbsjsn <shin.kirk@gmail.com>
2019-06-21 20:19:05 -0700

    completed CookieParser, working on middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,8 +1,30 @@
 const models = require('../models');
 const Promise = require('bluebird');
 
+const parseCookies = require('./cookieParser');
+
 module.exports.createSession = (req, res, next) => {
+  // do something
+  models.Sessions.create();
+  // .then to assign parsed cookies to req
+    // do everything below
+    // check if cookie is valid
+      // if not throw
+      // if yes ----- NOT SURE YET
+
+  console.log(req);
+  // look up user data related to session
+  // assigns object to session property on req
+  // contains user information
+    // userId
+    // hash - unique hash of session
+
+  // .catch the invalid cookie
+    // send them to the login page
 };
+
+// MORE EXTRA CREDIT: do inner join of users & session to collect user specific info
+// EXTRA CREDIT: MOUNT THIS FUNCTION TO APP.JS
 
 /************************************************************/
 // Add additional authentication middleware functions below
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,12 +1,17 @@
 const parseCookies = (req, res, next) => {
   let result = {};
-  console.log('this is req------------------------------',req.headers);
-  // return req.headers.cookies;
-  // split string by ;
-  // split each array index by '='
-  // put together as object
-  console.log(typeof req.headers.cookies);
-  
+
+  var cookieObj = req.headers.cookie;
+  if(cookieObj != undefined) {
+    var cookieBySemi = cookieObj.split('; ');
+    for(var entry of cookieBySemi) {
+      var cookie = entry.split('=');
+      result[cookie[0]] = cookie[1];
+    }
+  }
+  req.cookies = result;
+  next();
+
 };
 
 module.exports = parseCookies;
