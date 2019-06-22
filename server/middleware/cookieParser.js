fc084eff64c6cb6f922a0df2714f721336e21c38
kbsjsn <shin.kirk@gmail.com>
2019-06-21 17:34:22 -0700

    Wrote pseudo code for how to parse cookies

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,5 +1,12 @@
 const parseCookies = (req, res, next) => {
-    
+  let result = {};
+  console.log('this is req------------------------------',req.headers);
+  // return req.headers.cookies;
+  // split string by ;
+  // split each array index by '='
+  // put together as object
+  console.log(typeof req.headers.cookies);
+  
 };
 
 module.exports = parseCookies;
